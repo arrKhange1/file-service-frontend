3cddb4cee7780289d9991f9be7ab7fcef8a63207
arrKhange1 <therock65frostik@gmail.com>
2024-12-15 23:55:33 +0500

    refact: update eslint config with proper linter for react

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import pluginReact from "eslint-plugin-react-refresh";
 import eslintPrettier from "eslint-config-prettier";
 
 
@@ -11,6 +11,6 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.recommended,
   eslintPrettier,
 ];
--- src/pages/partition/ui/PartitionPage.tsx
+++ src/pages/partition/ui/PartitionPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const PartitionPage = () => {
   return <>Its PartitionPage</>;
 };
