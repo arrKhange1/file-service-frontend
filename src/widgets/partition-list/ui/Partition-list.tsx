21afd6e05428b19252f755cb9637e9eeef04646c
arrKhange1 <therock65frostik@gmail.com>
2024-12-18 20:35:29 +0500

    feat: add global colors

--- src/widgets/partition-list/ui/Partition-list.tsx
+++ src/widgets/partition-list/ui/Partition-list.tsx
@@ -10,7 +10,13 @@ export const PartitionList = () => {
         expandIcon={<CollapseExpandIcon type={'expand'} width={6} height={9} />}
         collapseIcon={<CollapseExpandIcon type={'collapse'} width={6} height={9} />}
       >
-        <AccordionTab header={<div className={styles.listHeader}>Your dropbox</div>}>
+        <AccordionTab
+          header={
+            <div className={styles.listHeader} data-header-list>
+              Your dropbox
+            </div>
+          }
+        >
           <DirectoryItem isActive={true} name="Dir1" />
           <DirectoryItem isActive={false} name="Директория 2" />
         </AccordionTab>
