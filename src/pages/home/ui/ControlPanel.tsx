1abe7e5158db685ce210d653f18714a526d8f081
arrKhange1 <therock65frostik@gmail.com>
2024-12-18 21:29:26 +0500

    refact: change expand/collapse bgcolor to original

--- src/pages/home/ui/ControlPanel.tsx
+++ src/pages/home/ui/ControlPanel.tsx
@@ -1,11 +1,8 @@
-import { DirectoryItem } from '../../../entities/ui/DirectoryItem';
 import { PartitionList } from '../../../widgets/partition-list/ui/PartitionList';
 
 export const ControlPanel = () => {
   return (
     <>
-      <DirectoryItem name="events" isActive={true} />
-      <DirectoryItem name="products" isActive={false} />
       <PartitionList />
     </>
   );
