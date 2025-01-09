a016e7b38f72284c01ecac4200677e743eeb6a00
arrKhange1 <therock65frostik@gmail.com>
2025-01-09 14:12:04 +0500

    feat: add rhf, bind rhf to directory form

--- src/features/add-node-form/ui/add-directory-form/AddDirectoryForm.tsx
+++ src/features/add-node-form/ui/add-directory-form/AddDirectoryForm.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Input } from '../../../../shared/ui/input/Input';
 import { AllNodesContext } from '../../../../widgets/all-nodes-table/store/all-nodes-context';
 import styles from '../AddNodeForm.module.scss';
+import { Controller, useForm } from 'react-hook-form';
+import { DirectoryNode } from '../../../../shared/api/fs-nodes/fs-nodes.model';
+
+type DirectoryForm = Pick<DirectoryNode, 'name'>;
 
 interface AddDirectoryFormProps {
   onHide: () => void;
@@ -9,10 +13,28 @@ interface AddDirectoryFormProps {
 
 export const AddDirectoryForm: React.FC<AddDirectoryFormProps> = ({ onHide }) => {
   const ctx = React.useContext(AllNodesContext);
-  console.log(ctx);
+  const { control, handleSubmit } = useForm<DirectoryForm>({
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  function onSubmit(directoryForm: DirectoryForm): void {
+    console.log(directoryForm);
+
+    onHide();
+  }
+
   return (
-    <form className={styles.form} onSubmit={() => onHide()}>
-      <Input type="text" placeholder="Название директории..." />
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="name"
+        control={control}
+        render={({ field: { value: name, onChange } }) => (
+          <Input type="text" placeholder="Название директории..." onChange={onChange} value={name} />
+        )}
+      />
+
       <Input className={styles.submitBtn} type="submit" value="Добавить" />
     </form>
   );
