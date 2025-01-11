41dd4429f1fdfef9987b4945cc0027e5e4c0f09e
arrKhange1 <therock65frostik@gmail.com>
2025-01-11 21:45:35 +0500

    feat: add custom flux store on context for table data

---
+++ src/entities/file-system-node-table/model/file-system-nodes-context.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { FileSystemNodeWithSubRows } from '../../../shared/api/fs-nodes/fs-nodes.model';
+
+type AddNodePayload = { id: string; nodeToAdd: FileSystemNodeWithSubRows };
+type DeleteNodePayload = { id: string };
+type PatchNodePayload = { id: string; nodePatch: Partial<FileSystemNodeWithSubRows> };
+type Action =
+  | { type: 'addNode'; payload: AddNodePayload }
+  | { type: 'deleteNode'; payload: DeleteNodePayload }
+  | { type: 'patchNode'; payload: PatchNodePayload };
+type Dispatch = (action: Action) => void;
+type State = { data: FileSystemNodeWithSubRows[]; selectedNode: FileSystemNodeWithSubRows | null };
+type FileSystemNodesProviderProps = { defaultState?: State; children: React.ReactNode };
+
+const FileSystemNodesContext = React.createContext<{ state: State; dispatch: Dispatch } | undefined>(undefined);
+
+function fileSystemNodesReducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'addNode': {
+      function nodeUpdater(
+        rows: FileSystemNodeWithSubRows[],
+        id: string,
+        nodeToAdd: FileSystemNodeWithSubRows,
+      ): FileSystemNodeWithSubRows[] {
+        if (!rows.length) return [];
+        const directoryNode = rows.find((row) => row._id === id);
+        const partitionNode = rows.find((row) => row.parentId === id);
+        if (directoryNode) {
+          const rowWithAddedNode: FileSystemNodeWithSubRows = {
+            ...directoryNode,
+            subRows: [...(directoryNode.subRows ?? []), nodeToAdd],
+          };
+          return rows.map((row) => (row._id === id ? rowWithAddedNode : row));
+        } else if (partitionNode) return [...rows, nodeToAdd];
+        return rows.map((row) => (row.subRows ? { ...row, subRows: nodeUpdater(row.subRows, id, nodeToAdd) } : row));
+      }
+      const updatedData = nodeUpdater(state.data, action.payload.id, action.payload.nodeToAdd);
+      return { ...state, data: updatedData };
+    }
+    case 'deleteNode': {
+      function nodeUpdater(rows: FileSystemNodeWithSubRows[], id: string): FileSystemNodeWithSubRows[] {
+        if (!rows.length) return [];
+        const searchingRow = rows.find((row) => row._id === id);
+        if (searchingRow !== undefined) return rows.filter((row) => row._id !== id);
+        return rows.map((row) => (row.subRows ? { ...row, subRows: nodeUpdater(row.subRows, id) } : row));
+      }
+
+      const updatedData = nodeUpdater(state.data, action.payload.id);
+      return { ...state, data: updatedData };
+    }
+    case 'patchNode': {
+      function nodeUpdater(
+        rows: FileSystemNodeWithSubRows[],
+        id: string,
+        nodePatch: Partial<FileSystemNodeWithSubRows>,
+      ): FileSystemNodeWithSubRows[] {
+        if (!rows.length) return [];
+        const searchingRow = rows.find((row) => row._id === id);
+        if (searchingRow !== undefined) return rows.map((row) => (row._id === id ? { ...row, ...nodePatch } : row));
+        return rows.map((row) => (row.subRows ? { ...row, subRows: nodeUpdater(row.subRows, id, nodePatch) } : row));
+      }
+
+      const updatedData = nodeUpdater(state.data, action.payload.id, action.payload.nodePatch);
+      return { ...state, data: updatedData };
+    }
+  }
+}
+
+const FileSystemNodesProvider: React.FC<FileSystemNodesProviderProps> = ({ defaultState, children }) => {
+  const [state, dispatch] = React.useReducer(fileSystemNodesReducer, defaultState ?? { data: [], selectedNode: null });
+  const value = { state, dispatch };
+  return <FileSystemNodesContext.Provider value={value}>{children}</FileSystemNodesContext.Provider>;
+};
+
+function useFileSystemNodes() {
+  const ctx = React.useContext(FileSystemNodesContext);
+  if (ctx === undefined) throw new Error('useFileSystemNodes should be used with FileSystemNodesProvider');
+  return ctx;
+}
+
+export { FileSystemNodesProvider, useFileSystemNodes };
