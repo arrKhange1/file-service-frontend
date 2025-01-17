51b3212a4d0557e7e5a7913b295daa9d2b69c611
arrKhange1 <therock65frostik@gmail.com>
2025-01-17 22:00:17 +0500

    refact: change prime accordion to self-written

--- src/shared/ui/accordion/Accordion.tsx
+++ src/shared/ui/accordion/Accordion.tsx
@@ -1,4 +1,3 @@
-import { CollapseExpandIcon } from '../../icons/collapse-expand-icon/CollapseExpandIcon';
 import { PropsWithChildren, useState } from 'react';
 import styles from './Accordion.module.scss';
 
@@ -7,19 +6,11 @@ interface AccordionProps {
 }
 
 export const Accordion = ({ children, headerText }: PropsWithChildren<AccordionProps>) => {
-  const [contentCollapsed, setContentCollapsed] = useState(true);
+  const [contentCollapsed, toggleContentCollapsed] = useState(true);
   return (
-    // <Accordion
-    //   expandIcon={<CollapseExpandIcon type={'expand'} />}
-    //   collapseIcon={<CollapseExpandIcon type={'collapse'} />}
-    //   className={styles.list}
-    // >
-    //   <AccordionTab header={<div className={styles.listHeader}>{headerText}</div>}>{children}</AccordionTab>
-    // </Accordion>
     <section className={styles.accordion}>
-      <header className={styles.accordionHeader}>
-        {contentCollapsed && <span onClick={() => setContentCollapsed(false)}>Expand</span>}
-        {!contentCollapsed && <span onClick={() => setContentCollapsed(true)}>Collapse</span>}
+      <header className={styles.accordionHeader} onClick={() => toggleContentCollapsed((prev) => !prev)}>
+        <img className={styles.accordionCollapse} src="src/app/icons/collapse.svg" />
         <span>{headerText}</span>
       </header>
       <section className={styles.accordionContent}>{!contentCollapsed && children}</section>
