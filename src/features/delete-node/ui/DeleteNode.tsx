193d5e88225cbcdaa233548f71d3c9079a2a2b22
arrKhange1 <therock65frostik@gmail.com>
2025-01-17 20:17:55 +0500

    feat: add delete node feature

---
+++ src/features/delete-node/ui/DeleteNode.tsx
@@ -0,0 +1,20 @@
+import { useFileSystemNodes } from '../../../entities/file-system-node-table/model/file-system-nodes-context';
+import { FileSystemNodeService } from '../../../shared/api/fs-nodes/fs-nodes.service';
+import { Input } from '../../../shared/ui/input/Input/Input';
+
+interface DeleteNodeProps {}
+
+export const DeleteNode: React.FC<DeleteNodeProps> = () => {
+  const {
+    state: { selectedNode },
+    dispatch,
+  } = useFileSystemNodes();
+
+  async function deleteNode() {
+    if (!selectedNode) return;
+    await FileSystemNodeService.deleteNodeById({ id: selectedNode._id });
+    dispatch({ type: 'deleteNode', payload: { id: selectedNode._id } });
+  }
+
+  return <Input type="button" value="Delete node" onClick={deleteNode} />;
+};
--- src/widgets/all-nodes-table/ui/header/Header.tsx
+++ src/widgets/all-nodes-table/ui/header/Header.tsx
@@ -2,6 +2,7 @@ import styles from './Header.module.scss';
 import { AddNode } from '../../../../features/add-node/ui/AddNode';
 import { EditNode } from '../../../../features/edit-node/EditNode';
 import { useFileSystemNodes } from '../../../../entities/file-system-node-table/model/file-system-nodes-context';
+import { DeleteNode } from '../../../../features/delete-node/ui/DeleteNode';
 
 interface HeaderProps {}
 
@@ -16,6 +17,7 @@ export const Header: React.FC<HeaderProps> = () => {
         <section className={styles.tableFeatures}>
           {selectedNode?.type !== 'FILE' && <AddNode />}
           {selectedNode && <EditNode />}
+          {selectedNode && <DeleteNode />}
         </section>
       </section>
     </>
