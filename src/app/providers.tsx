601b2083787a1144a5d9a2c1ead160a3aba95cd4
arrKhange1 <therock65frostik@gmail.com>
2025-01-18 13:49:45 +0500

    refact: remove redundant dependencies

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from './router';
-import { PrimeReactProvider } from 'primereact/api';
 
 interface ProvidersProps {
   queryClient: QueryClient;
@@ -9,9 +8,7 @@ interface ProvidersProps {
 export const Providers = ({ queryClient }: ProvidersProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <PrimeReactProvider>
-        <RouterProvider />
-      </PrimeReactProvider>
+      <RouterProvider />
     </QueryClientProvider>
   );
 };
